9e428ea76a1e0a762af037d498e933f181461119
aparks4 <arynmparks@gmail.com>
2022-09-20 12:04:11 -0700

    nav bar ony making one request for user data

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,20 +4,25 @@ import SpotifyWebApi from 'spotify-web-api-node';
 export default function NavBar({accessToken}) {
     const [currentUser, setCurrentUser] = useState();
 
-    const spotifyApi = new SpotifyWebApi({
+    useEffect(() => {
+        const spotifyApi = new SpotifyWebApi({
             clientId: "de0242264777412ea8c3adc7e7c63029",
-            accessToken: accessToken
-        })
-    
-    spotifyApi.getMe()
+            accessToken
+        });
+        
+        
+        spotifyApi.getMe()
         .then(function(data) {
             console.log('my user data: ', data.body);
             setCurrentUser(data.body);
         }, function(err) {
             console.log(err)
         });
+    }, [accessToken])
     
     
+
+ 
     if(!currentUser) return
     return(
         <a href={currentUser.external_urls.spotify}>{currentUser.display_name}</a>
