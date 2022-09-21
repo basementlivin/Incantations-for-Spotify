a07abeb35d6c86f38b6d95edcd6d0b3c2196f96b
aparks4 <arynmparks@gmail.com>
2022-09-21 13:50:25 -0700

    INCANTATION SUBMIT FUNCTION WORKING

--- src/components/Incantations.js
+++ src/components/Incantations.js
@@ -3,6 +3,7 @@ import SpotifyWebApi from 'spotify-web-api-node';
 import { useState, useEffect } from 'react';
 
 
+
 export default function Incantations ({accessToken}) {
     const [form, setForm] = useState("");
 
@@ -12,45 +13,40 @@ export default function Incantations ({accessToken}) {
     }
     const handleSubmit = async (e, ) => {
         e.preventDefault();
+        let spotifyApi;
+        const incantation = {...form};
+        let tracks = [];
+        let id = '';
+        const keyWords = incantation.incantation.split(" ");
         try {
-            const incantation = {...form};
-            let tracks = [];
-            let id = '';
-            const keyWords = incantation.incantation.split(" ");
             console.log(keyWords);
-            const spotifyApi = new SpotifyWebApi({
+            spotifyApi = new SpotifyWebApi({
                 clientId: "de0242264777412ea8c3adc7e7c63029",
                 clientSecret: "3e44db334b744416b587c80314f5745f",
                 accessToken: accessToken
             });
-
-            spotifyApi.createPlaylist(`${incantation.incantation}`, {
+            const data = await spotifyApi.createPlaylist(`${incantation.incantation}`, {
                 'description': 'Created over a piping hot cauldron with the incantations for spotify app🪄', 
                 'public': 'true',
-            }).then(function(data) {
-                id = data.body.id;
-                console.log('Created playlist!', id);
-            }, function(err) {
-                console.log(err);
-            })
-            .then(keyWords.map((word) => {
-                spotifyApi.searchTracks(`${word}`)
-                .then(function(data) {
-                    tracks.push(data.body);
-                }, function(err) {
-                    console.log(err);
-                });
-            }))
-            .then(spotifyApi.addTracksToPlaylist(`spotify:playlist:${id}`, [tracks])
-            .then(function(data) {
-                console.log(id);
-                console.log('Added tracks!');
-            }, function(err) {
-                console.log(err);
-            }))
+            });
+            id = data.body.id;
+            console.log('created playlist!', id);
+        } catch(err) {
+            console.log(err);
+        }
+        try {
+            keyWords.map(async (word) => {
+               let data = await spotifyApi.searchTracks(`${word}`);
+                for(let i = 0; i < data.body.tracks.items.length; i++) {
+                tracks.push(data.body.tracks.items[i].uri);
+               }
+               console.log('43: ', tracks);
+               spotifyApi.addTracksToPlaylist(id, tracks);
+               console.log(`${tracks[0]}`);
+        })
         } catch(err) {
             console.log(err)
-        } 
+        }
     }
     return (
         <>
